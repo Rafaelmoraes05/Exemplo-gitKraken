a4421d8d285fbf4fbe7d2816e6ecb560b012538e
Rafael Moraes <rafaelmoraes005@gmail.com>
2023-10-09 15:54:51 -0300

    initial commit

---
+++ src/Exemplo/ExemploCommit.java
@@ -0,0 +1,9 @@
+package Exemplo;
+
+public class ExemploCommit {
+
+	public static void main(String[] args) {
+		System.out.println("Citando a arvore");
+	}
+
+}
---
+++ src/Exemplo/HelloKraken.java
@@ -0,0 +1,9 @@
+package Exemplo;
+
+public class HelloKraken {
+
+	public static void main(String[] args) {
+		System.out.println("Hello word git Kraken");
+	}
+
+}
