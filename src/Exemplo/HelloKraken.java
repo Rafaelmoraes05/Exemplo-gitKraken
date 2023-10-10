945d6bbba5b7b94733236ea62a8c70b3763b554e
Rafael Moraes <rafaelmoraes005@gmail.com>
2023-10-10 14:51:46 -0300

    esse commit

--- src/Exemplo/HelloKraken.java
+++ src/Exemplo/HelloKraken.java
@@ -4,7 +4,7 @@ public class HelloKraken {
 
 	public static void main(String[] args) {
 		System.out.println("Hello word git");
-		System.out.println("Essa vai");
+		System.out.println("Esse commit");
 	}
 
 }
